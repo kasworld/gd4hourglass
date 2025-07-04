a4f796ee1aa11e97359ff7c5a513f96d04fb90d9
Kang Seukwon <kasworld@gmail.com>
2025-07-04 14:11:40 +0900

    rename to glass cube

--- glass_cube.gd
+++ glass_cube.gd
@@ -1,14 +1,14 @@
 extends StaticBody3D
-class_name GlassPlane
+class_name GlassCube
 
 func _ready() -> void:
 	set_color( Color(NamedColorList.color_list.pick_random()[0], 0.5) )
 
-func set_material(mat :Material) -> GlassPlane:
+func set_material(mat :Material) -> GlassCube:
 	$MeshInstance3D.mesh.material = mat
 	return self
 
-func set_color(co :Color) -> GlassPlane:
+func set_color(co :Color) -> GlassCube:
 	$MeshInstance3D.mesh.material.albedo_color = co
 	return self
 
